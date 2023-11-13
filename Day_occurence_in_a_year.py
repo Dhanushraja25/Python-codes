6ccc86efffab1850edd895bf7545d34381c9bf20
DHANUSHRAJA <103551618+Dhanushraja25@users.noreply.github.com>
2023-11-14 00:42:33 +0530

    Update and rename Day occurence in a year.py to Day_occurence_in_a_year.py

--- Day_occurence_in_a_year.py
+++ Day_occurence_in_a_year.py
@@ -1,6 +1,3 @@
-# python program Find number of
-# times every day occurs in a Year 
-
 
 import datetime 
 import calendar
@@ -12,19 +9,16 @@ def day_occur_time(year):
 		"Thursday", "Friday", "Saturday", 
 		"Sunday" ]
 	
-	# Initialize all counts as 52
+
 	L = [52 for i in range(7)]
 	
-	# Find the index of the first day
-	# of the year
 	pos = -1
 	day = datetime.datetime(year, month = 1, day = 1).strftime("%A")
 	for i in range(7):
 		if day == days[i]:
 			pos = i
 			
-	# mark the occurrence to be 53 of 1st day
-	# and 2nd day if the year is leap year
+
 	if calendar.isleap(year):
 		L[pos] += 1
 		L[(pos+1)%7] += 1
@@ -33,7 +27,6 @@ def day_occur_time(year):
 		L[pos] += 1
 		
 	
-	# Print the days
 	for i in range(7):
 		print(days[i], L[i])
 	
